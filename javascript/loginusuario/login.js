5ff48b536bbddda8fdf338d250b9ecae3a3267a6
pacolopez16 <116556032+pacolopez16@users.noreply.github.com>
2024-01-15 15:45:44 +0100

    Add files via upload

---
+++ javascript/loginusuario/login.js
@@ -0,0 +1,10 @@
+
+
+function login(){
+    var userx 
+    var usery
+    let mensaje = userx + usery;
+     userx = document.getElementById("info1").value;
+     usery = document.getElementById("info2").value;
+     mensaje = document.getElementById("mensaje").innerHTML = "Bienvenido "+userx+" con la contraseña "+usery;
+}
