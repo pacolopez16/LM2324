ed084df6511e4f60c9a25e593fd832105ef140df
pacolopez16 <116556032+pacolopez16@users.noreply.github.com>
2024-01-15 11:48:26 +0100

    Add files via upload

---
+++ javascript/ejercicio1/ej1.js
@@ -0,0 +1,10 @@
+
+
+function login(){
+    var infox 
+    var infoy
+    let mensaje = infox + infoy;
+     datox = document.getElementById("user1").value;
+     datoy = document.getElementById("user2").value;
+     mensaje = document.getElementById("mensaje").innerHTML = "Bienvenido "+infox+" con la contraseña "+infoy;
+}
